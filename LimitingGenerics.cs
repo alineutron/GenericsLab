8b9cf676f0db712dd8f3048ca22d491cc859a382
alineutron <asad.mirza@live.com>
2016-12-12 10:52:22 +0100

    limiting the generics to types

---
+++ LimitingGenerics.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace GenericsLab
+{
+	public class LimitingGenerics
+	{
+		public void tryIt()
+		{
+			//error var employee = new SuperPerson<ExEmployee>();
+			var employee = new SuperPerson<CurrentEmployee>();
+		}
+	}
+
+	public class CurrentEmployee
+	{
+		private string name;
+		private int id;
+
+		public CurrentEmployee(string s, int i)
+		{
+			name = s;
+			id = i;
+		}
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		public int ID
+		{
+			get { return id; }
+			set { id = value; }
+		}
+	}
+
+	public class ExEmployee
+	{
+		private string name;
+		private int id;
+
+		public ExEmployee(string s, int i)
+		{
+			name = s;
+			id = i;
+		}
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		public int ID
+		{
+			get { return id; }
+			set { id = value; }
+		}
+	}
+
+	public class SuperPerson<T> where T : CurrentEmployee
+	{
+		public SuperPerson()
+		{
+			Console.WriteLine("I am a super employee");
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,27 @@ namespace GenericsLab
 	{
 		static void Main(string[] args)
 		{
+			System.Console.WriteLine("Hello stranger!");
+			System.Console.WriteLine("Press 1 to try: Limiting generics to certain types");
+
+			var input = System.Console.ReadLine();
+			switch (input)
+			{
+				case "1":
+				{
+
+						break;
+				}
+				default:
+				{
+					break;
+				}
+			}
+			System.Console.ReadLine();
 		}
 	}
+
+
+	
+
 }
